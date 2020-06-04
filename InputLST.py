e0138258aade43d3e1905b6d3d281dc18fbdae57
MHarazim <MHarazim@users.noreply.github.com>
2020-06-04 11:28:17 +0200

    Create InputLST.py

---
+++ InputLST.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 11:16:55 2020
+
+@author: manta
+"""
+# caseX= [freq, bandwidth, gain]
+
+#Sband
+case0 = [2.4e9, 6e6 , 3]
+case1 = [2.4e9, 6e6 , 5]
+case2 = [2.4e9, 6e6 , 6.5]
+case3 = [2.4e9, 6e6 , 8]
+
+#UHF
+case4 = [500e6, 50e3 , 1.5]
+case5 = [500e6, 50e3 , 5]
+case6 = [500e6, 50e3 , 8]
+## xband
+case7 = [10e9, 70e6 , 5]
+case8 = [10e9, 70e6 , 6.5]
+case9 = [10e9, 70e6 , 8]
+
+case10 = [10e9, 100e6 , 5]
+case11 = [10e9, 100e6 , 6.5]
+case12 = [10e9, 100e6 , 8]
+
+
+caselst = [case0, case1, case2, case3, case4, case5, case6, case7, case8, case9, case10, case11, case12]
+
+# Modulation schemes:
+
+
+
+
+#caselst = [case1, case2, case3, case4,case5)
+
+
+
+#for i in range(len(caselst)):
