54caf8e11c1ebb65f2a64099000bf74d2de569d9
3carlitosbrown <carlos.fortunylombrana@gmail.com>
2020-06-08 16:00:13 +0200

    Update DistanceLatitude.py

--- DistanceLatitude.py
+++ DistanceLatitude.py
@@ -5,5 +5,6 @@ def DistanceLatitude(width,altitude,latitude):
     Re =6378;
     #Number of orbits
     p = round(m.pi * Re / (2 * width))
+    #Distance in km
     d = 2 * (Re + altitude) * np.cos(latitude) * np.sin(np.pi / (2 * p))
     return d, p
